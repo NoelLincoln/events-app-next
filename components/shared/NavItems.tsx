819f4db18536f5137a7989f2538eb92573385100
Noel Bryant <noelsobryant@gmail.com>
2024-04-19 22:18:32 +0300

    dynamically load links and set active class

--- components/shared/NavItems.tsx
+++ components/shared/NavItems.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
-
+'use client';
+import React from 'react';
+import { headerLinks } from '@/constants';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 const NavItems = () => {
+  const pathname = usePathname();
   return (
-    <ul className='flex '>NavItems</ul>
-  )
-}
+    <ul className="md:flex-between flex w-full flex-col items-start gap-5 md:flex-row">
+      {headerLinks.map((link) => {
+        const isActive = pathname === link.route;
+        return (
+          <li
+            key={link.route}
+            className={`${
+              isActive && 'text-primary-500'
+            } flex-center p-medium-16 whitespace-nowrap`}
+          >
+            <Link href={link.route}>{link.label}</Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
 
-export default NavItems
+export default NavItems;
