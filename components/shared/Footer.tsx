71c71ec8dddfb1b568acf0f8688f1dbe44ba3e2b
Noel Bryant <noelsobryant@gmail.com>
2024-04-19 22:24:13 +0300

    add footer

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,7 +1,19 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import logo from '@/public/assets/images/logo.png';
+
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="border-t">
+      <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
+        <Link href="/">
+          <Image src={logo} alt="logo" width={128} height={38} />
+        </Link>
 
-export default Footer
+        <p>2024 TechTide MeetUps. All Rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
