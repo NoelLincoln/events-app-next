3bc495de0819838c587f9fffa4e0bd32d1265b20
Noel Bryant <noelsobryant@gmail.com>
2024-05-06 23:49:07 +0300

    Fix useForm module not found.

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { zodResolver } from '@hookform/resolvers/zod';
-// import { useForm } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 import * as z from 'zod';
 import { Button } from '@/components/ui/button';
 import {
