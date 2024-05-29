1f40d726ca0423997735fadac3f93f2edc4d93c6
Noel Bryant <noelsobryant@gmail.com>
2024-05-29 03:08:59 +0300

    Set userid to json object in updateparams.

--- app/(root)/events/[id]/update/page.tsx
+++ app/(root)/events/[id]/update/page.tsx
@@ -5,7 +5,7 @@ import { auth } from '@clerk/nextjs';
 const UpdateEvent = () => {
   const { sessionClaims } = auth();
 
-  const userId = sessionClaims?.userId as string;
+  const userId = sessionClaims?.userId as { userid: string };
   return (
     <>
       <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
