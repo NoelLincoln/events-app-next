5ba6ca9baf8883349cbee751065b5a740d767fc8
Noel Bryant <noelsobryant@gmail.com>
2024-06-20 22:01:16 +0300

    Fix linter error,properly pass props from checkout button to checkout component.

--- components/shared/Checkout.tsx
+++ components/shared/Checkout.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import { IEvent } from '@/lib/mongodb/database/models/event.model';
 
 const Checkout = ({ event, userId }: { event: IEvent; userId: string }) => {
-  return;
-  <>
-    <div>Checkout</div>;{event} {userId}
-  </>;
+  return (
+    <>
+      <div>Checkout</div>
+      <div>{event.title}</div>
+      <div>{userId}</div>
+    </>
+  );
 };
 
 export default Checkout;
