98f5737bb83ab873eb580413cf55304a7dc083dc
Noel Bryant <noelsobryant@gmail.com>
2024-07-21 23:04:11 +0300

    Fix import path for order model.

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -2,7 +2,7 @@ import Collection from '@/components/shared/Collection';
 import { Button } from '@/components/ui/button';
 import { getEventsByUser } from '@/lib/actions/event.actions';
 import { getOrdersByUser } from '@/lib/actions/order.actions';
-import { IOrder } from '@/lib/database/models/order.model';
+import { IOrder } from '@/lib/mongodb/database/order.model';
 import { SearchParamProps } from '@/types';
 import { auth } from '@clerk/nextjs';
 import Link from 'next/link';
