4ac61e5596ee5265a59f5a020f2f08016e80549c
Noel Bryant <noelsobryant@gmail.com>
2024-07-21 22:18:07 +0300

    Fix: price cannot be undefined TypeScript error.

--- lib/mongodb/database/models/event.model.ts
+++ lib/mongodb/database/models/event.model.ts
@@ -8,7 +8,7 @@ export interface IEvent extends Document {
   imageUrl: string;
   startDateTime: Date;
   endDateTime: Date;
-  price?: string;
+  price: string;
   isFree: boolean;
   url?: string;
   category: { _id: string; name: string };
