e63ff71aa81cca2c5b84b40d12c67cf4bc8929e0
Noel Bryant <noelsobryant@gmail.com>
2024-07-23 01:51:17 +0300

    Fix clerk error on signup. Set first and last name as required in clerk, set photo optional.

--- lib/mongodb/database/models/user.model.ts
+++ lib/mongodb/database/models/user.model.ts
@@ -4,9 +4,9 @@ const UserSchema = new Schema({
   clerkId: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   username: { type: String, required: true, unique: true },
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-  photo: { type: String, required: true },
+  firstName: { type: String, required: false },
+  lastName: { type: String, required: false },
+  photo: { type: String, required: false },
 });
 
 const User = models.User || model('User', UserSchema);
