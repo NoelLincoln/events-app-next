07a98642b3f53cd8363fcda1c20f28bec8fe738e
Noel Bryant <noelsobryant@gmail.com>
2024-07-23 07:10:10 +0300

    Enhance ui and layout.

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -30,7 +30,7 @@ const EventDetails = async ({
             alt="hero image"
             width={1000}
             height={1000}
-            className="h-full min-h-[300px] object-cover object-center"
+            className="h-[300px] w-full object-center md:h-[600px]"
           />
 
           <div className="flex w-full flex-col gap-8 p-5 md:p-10">
--- components/shared/Checkout.tsx
+++ components/shared/Checkout.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react';
 import { loadStripe } from '@stripe/stripe-js';
-
 import { IEvent } from '@/lib/mongodb/database/models/event.model';
 import { Button } from '../ui/button';
 import { checkoutOrder } from '@/lib/actions/order.actions';
@@ -17,12 +16,14 @@ const Checkout = ({ event, userId }: { event: IEvent; userId: string }) => {
 
     if (query.get('canceled')) {
       console.log(
-        'Order canceled -- continue to shop around and checkout when you’re ready.',
+        'Order canceled -- continue to shop around and checkout when you’re ready.'
       );
     }
   }, []);
 
-  const onCheckout = async () => {
+  const onCheckout = async (e: React.FormEvent) => {
+    e.preventDefault();
+
     const order = {
       eventTitle: event.title,
       eventId: event._id,
@@ -35,7 +36,7 @@ const Checkout = ({ event, userId }: { event: IEvent; userId: string }) => {
   };
 
   return (
-    <form action={onCheckout} method="post">
+    <form onSubmit={onCheckout}>
       <Button type="submit" role="link" size="lg" className="button sm:w-fit">
         {event.isFree ? 'Get Ticket' : 'Buy Ticket'}
       </Button>
